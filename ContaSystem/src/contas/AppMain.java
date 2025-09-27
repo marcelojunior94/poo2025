a1c11d0ac8dccc6e6b9ba5b29f1df503baf427a3
Marcelo Júnior <marcelojunior.job@gmail.com>
2025-09-27 16:36:30 -0300

    Add files via upload

--- ContaSystem/src/contas/AppMain.java
+++ ContaSystem/src/contas/AppMain.java
@@ -3,7 +3,7 @@ package contas;
 public class AppMain {
     public static void main(String[] args) {
         Conta conta01 = new Conta();
-        Conta qualquerNome = new Conta();
+        Conta conta02 = new Conta();
 
         //conta01
         conta01.nome = "Marcelo";
@@ -11,14 +11,31 @@ public class AppMain {
         conta01.numero = 152596753;
         conta01.saldo = 10000;
 
-        //qualquerNome
-        qualquerNome.nome = "Júnior";
-        qualquerNome.limite = 2500;
-        qualquerNome.numero = 753951456;
-        qualquerNome.saldo = 5000;
+        //conta02
+        conta02.nome = "Júnior";
+        conta02.limite = 2500;
+        conta02.numero = 753951456;
+        conta02.saldo = 5000;
 
-        System.out.println("Nome: " + conta01.nome + "\nLimite: "  + "R$ " + conta01.limite + "\nNúmero da conta: " + conta01.numero + "\nSaldo: " + conta01.saldo + " R$");
-        System.out.println("\n\nNome: " + qualquerNome.nome + "\nLimite: " + "R$ " + qualquerNome.limite + "\nNúmero da conta: " + qualquerNome.numero + "\nSaldo: " + qualquerNome.saldo + " R$");
+        System.out.println("Saldo antigo: ");
+        System.out.println("Conta 01: " + conta01.saldo);
+        System.out.println("Conta 02: " + conta02.saldo);
+
+        conta01.sacar(1000);
+        conta02.sacar(500);
+        System.out.println("\nSaldo novo pós saque: ");
+        System.out.println("Conta 01: " + conta01.saldo);
+        System.out.println("Conta 02: " + conta02.saldo);
+
+        conta01.depositar(300);
+        conta02.depositar(255);
+        System.out.println("\nSaldo novo pós depósito: ");
+        System.out.println("Conta 01: " + conta01.saldo);
+        System.out.println("Conta 02: " + conta02.saldo);
+
+
+        //System.out.println("Nome: " + conta01.nome + "\nLimite: "  + "R$ " + conta01.limite + "\nNúmero da conta: " + conta01.numero + "\nSaldo: " + "R$ " + conta01.saldo);
+        //System.out.println("\n\nNome: " + conta02.nome + "\nLimite: " + "R$ " + conta02.limite + "\nNúmero da conta: " + conta02.numero + "\nSaldo: " + "R$ "+ conta02.saldo);
 
         //System.out.println(conta01);
     }
--- ContaSystem/src/contas/Conta.java
+++ ContaSystem/src/contas/Conta.java
@@ -10,4 +10,19 @@ public class Conta {
         double novoSaldo = this.saldo - quantidade;
         this.saldo = novoSaldo;
     }
+
+    void depositar (double quantidade){
+        double novoSaldo = this.saldo + quantidade;
+        this.saldo = novoSaldo;
+    }
+
+    @Override
+    public String toString() {
+        return //"Conta{" +
+                "\nNumero = " + numero +
+                "\nNome = '" + nome + '\'' +
+                "\nSaldo = " + saldo +
+                "\nLimite = " + limite;
+                //'}';
+    }
 }
